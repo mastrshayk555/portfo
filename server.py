2d02f92eb42c1d6c6f3ff6718105fca9bfe7057c
mastrshayk555 <jeffreyheim55@gmail.com>
2020-11-15 15:29:14 -0500

    make updates

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
